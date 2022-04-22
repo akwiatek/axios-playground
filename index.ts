cc224da3e10d18128224f150d8d0f7c4f4205d86
Adam Kwiatek <kwiatek.adam@poczta.fm>
2022-04-23 00:57:31 +0200

    async

--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 (async function main() {
-  try {
-      const response = await axios.get('https://www.google.com/');
-      console.log(response);
-  } catch (error) {
-      console.log(error);
-  }
+    const response = await axios.get('https://www.google.com/');
+    console.log(response);
 })();
