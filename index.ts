cd876c4cf5102a4156620bee177891b5b90de58d
Adam Kwiatek <kwiatek.adam@poczta.fm>
2022-04-23 01:03:00 +0200

    prettier

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
+import _ from "lodash";
 
 (async function main() {
-    const response = await axios.get('https://www.google.com/');
-    console.log(response);
+  const response = await axios.get("https://www.google.com/");
+  console.log(`status: ${response.status}`);
+  console.log(`has data?: ${!_.isEmpty(response.data)}`);
 })();
