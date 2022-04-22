9ec9d3d2505f394d514f0752692044bde1bad1e0
Adam Kwiatek <kwiatek.adam@poczta.fm>
2022-04-23 01:06:07 +0200

    types

--- index.ts
+++ index.ts
@@ -2,9 +2,13 @@ import axios from "axios";
 import _ from "lodash";
 
 (async function main() {
+  await simple();
+})();
+
+async function simple(): Promise<void> {
   const response = await axios.get("https://www.google.com/");
   console.log(`
     status: ${response.status}
     has data?: ${!_.isEmpty(response.data)}
   `);
-})();
+}
