9ccbf16560ab4d5cef4f501473c0b2cab98eafce
Adam Kwiatek <kwiatek.adam@poczta.fm>
2022-04-24 23:45:24 +0200

    fluent api

--- server.ts
+++ server.ts
@@ -6,12 +6,12 @@ export function startServer(
   ip: string,
   port: number
 ): Pick<http.Server, "close"> {
-  let app = http.createServer((req, res) => {
-    console.log(`request: ${req.method}`);
-    res
-      .writeHead(200, { "Content-Type": "text/json" })
-      .end(JSON.stringify(sample));
-  });
-  app.listen(port, ip);
-  return app;
+  return http
+    .createServer((req, res) => {
+      console.log(`request: ${req.method}`);
+      res
+        .writeHead(200, { "Content-Type": "text/json" })
+        .end(JSON.stringify(sample));
+    })
+    .listen(port, ip);
 }
